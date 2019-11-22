6cd7371cf841807e3a9997543ee2c09afeb6badf
Staling R. Silverio N <silverstand@hotmail.com>
2019-11-22 14:58:56 -0300

    Primer commit

---
+++ api_pelis/api/views.py
@@ -0,0 +1,57 @@
+from .models import Pelicula, PeliculaFavorita
+from .serializers import PeliculaSerializer, PeliculaFavoritaSerializer
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets, views
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+class PeliculaViewSet(viewsets.ModelViewSet):
+  queryset = Pelicula.objects.all()
+  serializer_class = PeliculaSerializer
+
+class MarcarPeliculaFavorita(views.APIView):
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticated]
+
+  # POST -> Se usa para crear un recurso sin un identificador
+  # PUT -> Se usa para crear/reemplazar un recurso con un identificador
+
+  def post(self, request):
+
+    pelicula = get_object_or_404(
+      Pelicula, id=self.request.data.get('id', 0)
+    )
+
+    favorita, created = PeliculaFavorita.objects.get_or_create(
+      pelicula=pelicula, usuario=request.user
+    )
+
+    # Por defecto suponemos que se crea bien
+    content = {
+      'id': pelicula.id,
+      'favorita': True
+    }
+
+    # Si no se ha creado es que ya existe, entonces borramos el favorito
+    if not created:
+      favorita.delete()
+      content['favorita'] = False
+
+    return Response(content)
+
+class ListarPeliculasFavoritas(views.APIView):
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticated]
+
+  # GET -> Se usa para hacer lecturas
+
+  def get(self, request):
+
+    peliculas_favoritas = PeliculaFavorita.objects.filter(
+      usuario=request.user)
+    serializer = PeliculaFavoritaSerializer(
+      peliculas_favoritas, many=True)
+
+    return Response(serializer.data)
